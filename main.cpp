dcf5a2f2bd9ecf59f7653ad59517ea32f9ec8107
Md. Nazmul Alam <nazmulalamtuhin11011@gmail.com>
2024-05-06 23:11:27 +0600

    Changed

--- main.cpp
+++ main.cpp
@@ -1,72 +1,18 @@
 #include <SDL2/SDL.h>
-#include <stdio.h>
-#include <math.h>
-
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
-
-bool initializeSDL(SDL_Window** window, SDL_Renderer** renderer) {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        printf("SDL initialization failed: %s\n", SDL_GetError());
-        return false;
-    }
-
-    *window = SDL_CreateWindow("Circle Drawing", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (*window == NULL) {
-        printf("Window creation failed: %s\n", SDL_GetError());
-        return false;
-    }
-
-    *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (*renderer == NULL) {
-        printf("Renderer creation failed: %s\n", SDL_GetError());
-        return false;
-    }
-
-    return true;
-}
-
-void drawSolidCircle(SDL_Renderer* renderer, int centerX, int centerY, int radius) {
-    for (int x = -radius; x <= radius; x++) {
-        for (int y = -radius; y <= radius; y++) {
-            if (x*x + y*y <= radius*radius) {
-                SDL_RenderDrawPoint(renderer, centerX + x, centerY + y);
-            }
-        }
-    }
-}
+#include <SDL2/SDL_mixer.h>
 
 int main(int argc, char* argv[]) {
-    SDL_Window* window = NULL;
-    SDL_Renderer* renderer = NULL;
+    SDL_Init(SDL_INIT_AUDIO);
+    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+    Mix_Music *music = Mix_LoadMUS("assets/sound.mp3");
+    Mix_PlayMusic(music, -1);
 
-    if (!initializeSDL(&window, &renderer)) {
-        return 1;
+    while (Mix_PlayingMusic()) {
+        SDL_Delay(1000);
     }
 
-    SDL_Event event;
-    bool running = true;
-
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                running = false;
-            }
-        }
-
-        // Set background color to green
-        SDL_SetRenderDrawColor(renderer, 0,106,78, 255);
-        SDL_RenderClear(renderer);
-
-        // Draw solid circle in black
-        SDL_SetRenderDrawColor(renderer, 244,42,65, 255);
-        drawSolidCircle(renderer, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 100);
-
-        SDL_RenderPresent(renderer);
-    }
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+    Mix_FreeMusic(music);
+    Mix_CloseAudio();
     SDL_Quit();
 
     return 0;
