47021a00d1c36f71a7bf43dfe709c61369408ae0
Md. Nazmul Alam <nazmulalamtuhin11011@gmail.com>
2024-05-08 17:02:41 +0600

    Main file changed

--- main.cpp
+++ main.cpp
@@ -1,18 +1,51 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_mixer.h>
+#include <stdio.h>
 
-int main(int argc, char* argv[]) {
-    SDL_Init(SDL_INIT_AUDIO);
-    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
-    Mix_Music *music = Mix_LoadMUS("assets/sound.mp3");
-    Mix_PlayMusic(music, -1);
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
-    while (Mix_PlayingMusic()) {
-        SDL_Delay(1000);
+int main( int argc, char* args[] )
+{
+    //The window we'll be rendering to
+    SDL_Window* window = NULL;
+    
+    //The surface contained by the window
+    SDL_Surface* screenSurface = NULL;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    }
+    else
+    {
+        //Create window
+        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( window == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        }
+        else
+        {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface( window );
+
+            //Fill the surface white
+            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+            
+            //Update the surface
+            SDL_UpdateWindowSurface( window );
+
+            //Hack to get window to stay up
+            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+        }
     }
 
-    Mix_FreeMusic(music);
-    Mix_CloseAudio();
+    //Destroy window
+    SDL_DestroyWindow( window );
+
+    //Quit SDL subsystems
     SDL_Quit();
 
     return 0;
